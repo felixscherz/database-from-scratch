e4c5ad3ae85f3080a4f82f25e70a0f2335389ddd
Felix Scherz <felixwscherz@gmail.com>
2024-06-15 12:33:20 +0200

    feat: start with implementation

--- src/database/__init__.py
+++ src/database/__init__.py
@@ -1,5 +1,6 @@
 import struct
 from io import BytesIO
+from typing import BinaryIO
 
 MAGIC_BYTES = tuple(c.encode() for c in "magic")
 VERSION_BYTES = (0, 0, 1)
@@ -15,15 +16,27 @@ NUMBER_OF_COLUMNS_STRUCT = struct.Struct("<b")
 DATATYPE_STRUCT = struct.Struct("<b")
 
 DATATYPE_TO_BYTE = {int: 0, float: 1, str: 2, bytes: 3}
+BYTE_TO_DATATYPE = {0: int,  1:float, 2:str, 3:bytes}
 
 TYPE_OF_PAGE_STRUCT = struct.Struct("<b")
 INTERNAL_NODE = 0
 LEAF_NODE = 1
 
+class DecodeError(Exception):
+    def __init__(self, offset: int):
+        super().__init__(f"Invalid bytes encountered at {offset=}")
+
+
+def read_with(reader: BinaryIO, struct: struct.Struct):
+    return struct.unpack(reader.read(struct.size))
+
 
 class Database:
+    def __init__(self):
+        self.b = BytesIO()
+
     def create_table(self, name: str, schema, primary_key):
-        b = BytesIO()
+        b = self.b
         b.write(FILE_HEADER_MAGIC.pack(*MAGIC_BYTES))
         b.write(VERSION_BYTES_STRUCT.pack(*VERSION_BYTES))
         b.write(BYTES_PER_PAGE_STRUCT.pack(BYTES_PER_PAGE))
@@ -33,6 +46,7 @@ class Database:
             # <length-of-name><name><is-primary-key><datatype>
             b.write(struct.pack("<b", len(name)))
             b.write(struct.pack(f"<{len(name)}s", name.encode()))
+            # this could be changed to encode the key position 0,1,2,3 to allow for composite keys
             b.write(struct.pack("<?", name == primary_key))
             b.write(DATATYPE_STRUCT.pack(DATATYPE_TO_BYTE[datatype]))
 
@@ -47,7 +61,38 @@ class Database:
         b.write(struct.pack("<I", BYTES_PER_PAGE))  # point to first item in page
 
     def insert(self, item):
-
+        # when calling this, we might know nothing about the table, so we first need to parse the metaheader for
+        # information on the schema and where to begin traversing the b+ tree
         ...
 
     def read(self, key): ...
+
+    def table_info(self, name: str):
+        self._parse_meta_header(self.b)
+
+    def _parse_meta_header(self, reader: BinaryIO):
+        reader.seek(0)
+        if not read_with(reader, FILE_HEADER_MAGIC) == MAGIC_BYTES:
+            raise DecodeError(0)
+        version = read_with(reader, VERSION_BYTES_STRUCT)
+        bytes_per_page = read_with(reader, BYTES_PER_PAGE_STRUCT)
+        number_of_pages = read_with(reader, NUMBER_OF_PAGES_STRUCT)
+        number_of_columns = read_with(reader, NUMBER_OF_COLUMNS_STRUCT)
+        schema = {}
+        for _ in range(number_of_columns[0]):
+            name_length = read_with(reader, struct.Struct("<b"))
+            name = read_with(reader, struct.Struct(f"<{name_length[0]}s"))
+            is_primary_key = read_with(reader, struct.Struct("<?"))
+            datatype = read_with(reader, DATATYPE_STRUCT)
+            schema[name[0].decode()] = BYTE_TO_DATATYPE[datatype[0]]
+            
+        print(version)
+        print(bytes_per_page)
+        print(number_of_pages)
+        print(number_of_columns)
+        print(schema)
+
+
+
+
+
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,10 +1,29 @@
-from database import Database
+from io import BytesIO
+import struct
+from typing import BinaryIO
+from database import Database,MAGIC_BYTES, FILE_HEADER_MAGIC
 
 
 def test_read_after_write():
     db = Database()
     schema = {"id": int, "greeting": str}
     db.create_table("greetings", schema=schema, primary_key="id")
+    db.table_info("greetings")
     db.insert((0, "Hello World"))
 
     assert db.read(0) == ("Hello World")
+
+def test_struct():
+    a = FILE_HEADER_MAGIC.pack(*(b"m", b"a", b"g", b"i", b"c"))
+    print(a)
+
+    a = FILE_HEADER_MAGIC.pack(*(c.encode() for c in "magic"))
+
+    out = FILE_HEADER_MAGIC.unpack(a)
+    print(out)
+
+
+def test_buffer():
+    a = struct.pack("<b", 3)
+    out = struct.unpack("<b", a)
+    print(out)
