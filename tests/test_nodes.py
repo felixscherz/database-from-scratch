18721a64781aaecd0dbd6ae1cc80303128c923d0
Felix Scherz <felixwscherz@gmail.com>
2024-06-24 19:27:57 +0200

    test: add test for LeafNode

--- tests/test_nodes.py
+++ tests/test_nodes.py
@@ -1,6 +1,7 @@
 from io import BytesIO
 from database.nodes import InternalNode
 from database.nodes import MetaHeader
+from database.nodes import LeafNode
 
 
 def test_internal_node_can_be_serialized():
@@ -16,4 +17,18 @@ def test_internal_node_can_be_serialized():
     b.seek(0)
 
     assert InternalNode.read(b, root=True) == node
+
+def test_leaf_node_can_be_serialized():
+    meta_header = MetaHeader(
+        number_of_pages=1,
+        schema={"id": str},
+        primary_key=("id",),
+    )
+    node = LeafNode(items={}, meta_header=meta_header)
+
+    b = BytesIO()
+    node.write(b)
+    b.seek(0)
+
+    assert LeafNode.read(b, root=True) == node
 
